27d3492eac3274cc7b6ed7d757732d4bc01c9901
nlemast <nlemaster47@gmail.com>
2020-10-10 19:46:11 -0400

    Move generatePassword function declaration to global scope

--- generatePassword.js
+++ generatePassword.js
@@ -1,27 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const generateButton = document.querySelector('#generateButton');
+const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const SPECIAL_CHARACTERS = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
 
-    const generatePassword = () => {
-        const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const SPECIAL_CHARACTERS = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+const generateButton = document.querySelector('#generateButton');
+const newPasswordSpan = document.querySelector('#newPassword');
+const copyButton = document.querySelector('#copyButton');
 
-        const lengthDOM = document.querySelector('#lengthSelect');
-        const length = lengthDOM.options[lengthDOM.selectedIndex].text;
-        const specialCharsDOM = document.querySelector('#specialCharactersSelect');
-        const hasSpecialChars = specialCharsDOM.options[specialCharsDOM.selectedIndex].text;
+const generatePassword = () => {
+    const lengthDOM = document.querySelector('#lengthSelect');
+    const length = lengthDOM.options[lengthDOM.selectedIndex].text;
+    const specialCharsDOM = document.querySelector('#specialCharactersSelect');
+    const hasSpecialChars = specialCharsDOM.options[specialCharsDOM.selectedIndex].text;
 
-        const passwordChars = hasSpecialChars ? CHARACTERS + SPECIAL_CHARACTERS : CHARACTERS;
-        let password = '';
+    const passwordChars = hasSpecialChars ? CHARACTERS + SPECIAL_CHARACTERS : CHARACTERS;
+    let password = '';
 
-        for (let i = 0; i < Number(length); i++) {
-            const randomCharIndex = Math.round(Math.random() * passwordChars.length);
-            const randomChar = passwordChars[randomCharIndex];
+    for (let i = 0; i < Number(length); i++) {
+        const randomCharIndex = Math.round(Math.random() * passwordChars.length);
+        const randomChar = passwordChars[randomCharIndex];
 
-            password += randomChar;
-        }
+        password += randomChar;
+    }
 
-        return password;
-    };
+    return password;
+};
 
 const copyToClipboard = (password) => {
     const textarea = document.createElement('textarea');
